029f1748e453b33e91e2eaf04166d1803ddf4543
kmuramatsu <kmuramatsu@zynga.com>
2012-03-29 19:41:01 +0900

    とりあえずコミット。ログ見る限りFacebookエラー出てる

---
+++ client/TestAnywhere/src/com/testanywhere/Constants.java
@@ -0,0 +1,10 @@
+package com.testanywhere;
+
+public class Constants {
+	public final static int SPLASH_WAIT_TIME = 2000;
+	public final static String FACEBOOK_APP_ID = "330154980366566";
+	
+	// use for sharePref
+	public final static String FACEBOOK_ACCESS_TOKEN = "facebook_access_token";
+	public final static String FACEBOOK_ACCESS_EXPIRES = "facebook_access_expires";
+}
--- client/TestAnywhere/src/com/testanywhere/TestAnywhereActivity.java
+++ client/TestAnywhere/src/com/testanywhere/TestAnywhereActivity.java
@@ -1,51 +1,77 @@
 package com.testanywhere;
 
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
+
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.MotionEvent;
 
 public class TestAnywhereActivity extends Activity {
-	private boolean isTimerRun = true;
+	private Facebook facebook = null;
+	private SharedPreferences facebookPrefs = null;
 	@Override
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
+		setContentView(R.layout.main);
+		facebook = new Facebook(Constants.FACEBOOK_APP_ID);
+		facebookPrefs = getPreferences(MODE_PRIVATE);
+		String facebookAccessToken = facebookPrefs.getString(Constants.FACEBOOK_ACCESS_TOKEN, null);
+		
+		long facebookAcessExpires = facebookPrefs.getLong(Constants.FACEBOOK_ACCESS_EXPIRES, 0);
+		if(facebookAccessToken != null) {
+			facebook.setAccessToken(facebookAccessToken);
+		}
+		if(facebookAcessExpires != 0) {
+			facebook.setAccessExpires(facebookAcessExpires);
+		}
+		
+		/*
+		 * Only call authorize if the access_token has expired.
+		 */
+		if(!facebook.isSessionValid()) {
+		
+	    facebook.authorize(this, new String[] {}, new DialogListener() {
+	        @Override
+	        public void onComplete(Bundle values) {
+	            SharedPreferences.Editor editor = facebookPrefs.edit();
+	            editor.putString("access_token", facebook.getAccessToken());
+	            editor.putLong("access_expires", facebook.getAccessExpires());
+	                editor.commit();
+	            }
+	
+	            @Override
+	            public void onFacebookError(FacebookError error) {
+	            	System.out.println("facebook error");
+	            }
+	
+	            @Override
+	            public void onError(DialogError e) {
+	            	System.out.println("app error");
+	            }
+	
+	            @Override
+	            public void onCancel() {
+	            	System.out.println("cancel");
+	            }
+	        });
+	    }
 	}
+	
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("onResult");
+        facebook.authorizeCallback(requestCode, resultCode, data);
+    }
 	
 	public boolean onTouchEvent(MotionEvent event) {
-		if(isTimerRun) {
-			final Intent test = new Intent(this, TestAnywhereActivity.class);
-			test.setAction(test.ACTION_VIEW);
-			startActivity(test);
-			finish();
-		}
-		isTimerRun = false;
 		return true;
-	}
-	
-	public void onStop() {
-		super.onStop();
-		isTimerRun = false;
-	}
-	
-	public void onStart() {
-		super.onStart();
-		isTimerRun = true;
-		
-		final Intent test = new Intent(this, TestAnywhereActivity.class);
-		setContentView(R.layout.main);
-		Handler timerHandler = new Handler();
-		Runnable timerRunnable = new Runnable() {
-			public void run() {
-				if(isTimerRun) {
-					test.setAction(test.ACTION_VIEW);
-					startActivity(test);
-					finish();
-				}
-			}
-		};
-		timerHandler.postDelayed(timerRunnable, 3000);
-		
 	}
 }
