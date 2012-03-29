fbe47014d5e634433f58b9ee5bd487fe66785d95
kmuramatsu <kmuramatsu@zynga.com>
2012-03-29 17:44:51 +0900

    initial commit for client.

---
+++ client/TestAnywhere/src/com/testanywhere/TestAnywhereActivity.java
@@ -0,0 +1,13 @@
+package com.testanywhere;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestAnywhereActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
